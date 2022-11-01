a22033e94cd8c8dac60f39c4ec6b138c3302c382
aganowak <agnieszka.nowak@gmx.com>
2022-11-01 23:51:54 +0100

    application fetches data from mock server and displays events

--- helping-hands-app/src/App.js
+++ helping-hands-app/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
+import Events from './components/Events'
+import Navbar from './components/Navbar'
+import './App.css'
 
-function App() {
+const App = () => {
+  
+  const [events, setEvents] = useState([]);
+
+  //fetching events data from server as soon as the page loads
+  
+  useEffect(() => {
+    getEvents();
+  }, 
+  [])
+
+  const getEvents = async () => {
+    const eventsFromServer = await fetchEvents();
+    setEvents(eventsFromServer);
+  }
+
+  // Fetch all events
+  const fetchEvents = async () => {
+    console.log("fetching events")
+    const res = await fetch('https://20glq.mocklab.io/events')
+    const data = await res.json()
+    return data
+  }
+
+  const events_list_html = events.map((e) => <li key={e.ID}>{e.Title}</li>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className='navbar'>
+        <div className='navbar__title navbar__item'>Helping Hands</div>
+        <div className='navbar__item'>Register</div>
+        <div className='navbar__item'>Sign in</div>
+        <div className='navbar__item'>Contact Us</div>        
+      </nav>
+      <div className='container'>
+          <Header />
+          
+            
+          < Events events={events} />
+          
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
