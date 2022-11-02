c988bad2f276179c71ba18fd840e38f81830b8f2
aganowak <agnieszka.nowak@gmx.com>
2022-11-02 12:12:10 +0100

    nice display of cards

--- helping-hands-app/src/components/Events.js
+++ helping-hands-app/src/components/Events.js
@@ -3,13 +3,13 @@ import Event from './Event'
 
 const Events = ({ events }) => {
   return (
-    <>
+    <div className='events-container'>
       {events.map((event) => (
         <div className='event-card'>
             <Event key={event.ID} event={event} />
         </div>
       ))}
-    </>
+    </div>
   )
 }
 
