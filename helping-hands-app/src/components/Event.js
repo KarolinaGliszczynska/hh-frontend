138291fdac2a478320bd604f47027cf0f2c5fe8e
aganowak <agnieszka.nowak@gmx.com>
2022-11-02 10:58:53 +0100

    css added to event cards

--- helping-hands-app/src/components/Event.js
+++ helping-hands-app/src/components/Event.js
@@ -2,10 +2,9 @@ import React from 'react'
 
 const Event = ({ event }) => {
   return (
-    <div className="event-card">
-      <h5>{event.Category}</h5>
-      <span><h6>{event.Date_of_event}</h6></span>
-      <span><h6>{event.City}</h6></span>
+    <div>
+      <h5 className="event-card-category">{event.Category}</h5>
+      <h6><span>{event.Date_of_event}</span>&nbsp;<span>{event.City}</span></h6>
       <h3>
         {event.Title}{' '}
       </h3>
