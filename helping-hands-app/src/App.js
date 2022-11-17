387eb7380bb6e883eb7d1fe59382efb4e1473f07
Karolina Gliszczyńska <hello@karolinagliszczynska.com>
2022-11-17 10:11:32 +0100

    folder structure all events

--- helping-hands-app/src/App.js
+++ helping-hands-app/src/App.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Events from './components/Events'
+import Events from './components/AllEvents/Events'
 import Navbar from './components/navbar/Navbar'
 import Homepage from './components/main-page/homepage'
 import Login from './components/login'
--- helping-hands-app/src/components/AllEvents/Sidebar.js
+++ helping-hands-app/src/components/AllEvents/Sidebar.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { SidebarFilterItems } from "./SidebarFilterItems"
+
 
 const Sidebar = (props) => (
     <nav className='sidenav'>
