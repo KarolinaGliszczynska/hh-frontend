47e89e4425f5d2f561f2c976304796d4b36752da
Karolina Gliszczyńska <hello@karolinagliszczynska.com>
2022-11-17 13:22:42 +0100

    all event cads done

--- helping-hands-app/src/components/AllEvents/Event.js
+++ helping-hands-app/src/components/AllEvents/Event.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import pic from './eventpic.jpg'
+
 const Event = ({ event }) => {
+    const [year, month, day] = event.dateOfEvent.split('-')
+    const monthNames = ["JUN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+    const monthName = monthNames[month-1]
 
       return (
         <div>
           <h6 className="event-card-category">{event.eventCategory}</h6>
              <   img className='event-card-pic' src={pic} alt="event-pic" />
-          <div className='event-card-details'>
-          <h6><span>{event.dateOfEvent}</span>&nbsp;<span>{event.city}</span></h6>
-          <h3>
-            {event.eventTitle}{' '}
-          </h3>
-          <p>{event.eventDescription}</p>
+          <div className='event-card-details row'>
+                <div id='col1'  className='column'>
+                <h2>{day}</h2>
+                    <p>{monthName}</p>
+                    </div>
+              <div id='col2' className='column'>
+                <h4>
+                {event.eventTitle}{' '}
+                </h4>
+                 <p>{event.eventDescription}</p>
+              </div>
           </div>
         </div>
       )
