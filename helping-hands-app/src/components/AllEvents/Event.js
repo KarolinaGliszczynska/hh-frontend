8d48b38e5d1992173ef2e8625b61b23c7fb0630f
Karolina Gliszczyńska <hello@karolinagliszczynska.com>
2022-11-17 10:56:24 +0100

    event cards styled v1

--- helping-hands-app/src/components/AllEvents/Event.js
+++ helping-hands-app/src/components/AllEvents/Event.js
@@ -1,15 +1,18 @@
 import React from 'react'
-
+import pic from './eventpic.jpg'
 const Event = ({ event }) => {
 
       return (
         <div>
-          <h5 className="event-card-category">{event.eventCategory}</h5>
+          <h6 className="event-card-category">{event.eventCategory}</h6>
+             <   img className='event-card-pic' src={pic} alt="event-pic" />
+          <div className='event-card-details'>
           <h6><span>{event.dateOfEvent}</span>&nbsp;<span>{event.city}</span></h6>
           <h3>
             {event.eventTitle}{' '}
           </h3>
           <p>{event.eventDescription}</p>
+          </div>
         </div>
       )
 }
