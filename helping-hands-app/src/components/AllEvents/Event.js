763236a8f6ac6af88954523e6f34b43208bd1f9d
aganowak <agnieszka.nowak@gmx.com>
2022-11-23 11:23:08 +0100

    EventDetailCard displays default image if no event photo was returned from server

--- helping-hands-app/src/components/AllEvents/Event.js
+++ helping-hands-app/src/components/AllEvents/Event.js
@@ -5,13 +5,18 @@ const Event = ({ event }) => {
     const [year, month, day] = event.dateOfEvent.split('-')
     const monthNames = ["JUN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
     const monthName = monthNames[month-1]
+    const picture = null
 
       return (
         <div>
           <h6 className="event-card-category">{event.eventCategory}</h6>
-             <   img className='event-card-pic'
-                     src={pic} alt="event-pic" />
-          <div className='event-card-details row'>
+          <div>
+            { picture
+                ? <img className='event-card-pic' src={picture} alt="event-pic" />
+                : <img className='event-card-pic' src={pic} alt="event-pic" />
+            }
+          </div>
+            <div className='event-card-details row'>
                 <div id='col1'  className='column'>
                  <p>{year}</p>
                 <h2>{day}</h2>
--- helping-hands-app/src/components/EventDetailCard.js
+++ helping-hands-app/src/components/EventDetailCard.js
@@ -15,18 +15,14 @@ const EventDetailCard = ({ event }) => {
     }
 
      */
-    const picture = "data:image/png;base64," + event.image
+    const picture = event.image ? "data:image/png;base64," + event.image : pic;
 
     return (
         <div className='event-detail-card'>
             <h5 > {event.city}</h5>
             <h1> {event.eventTitle} </h1>
             <div>
-                { picture
-                  ? <img className='event-pic-large' src={picture} alt="event-pic" />
-                  : <img className='event-pic-large' src={pic} alt="event-pic" />
-                }
-
+                <img className='event-pic-large' src={picture} alt="event-pic" />
             </div>
             <p> {event.eventDescription}</p>
         </div>
