af348a611f15d02b9f4c7cc42089e4d6bcb29e9f
Iwona Zygma <iwona.zygma@gmail.com>
2023-01-09 12:20:02 +0100

    Saving event

--- helping-hands-app/src/setupProxy.js
+++ helping-hands-app/src/setupProxy.js
@@ -4,8 +4,13 @@ module.exports = function (app) {
     app.use(
         "/api",
         createProxyMiddleware({
-            target: "http://localhost:8080",
+            target: "http://localhost:8080/",
             changeOrigin: true,
+
+           pathRewrite: (path, req) => {
+                path = path.replace("/api/events", "/events");
+                return path;
+           }
         })
     );
 };
